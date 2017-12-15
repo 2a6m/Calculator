433bf9b2e01a15ca0aec8e02d9156d1132c4b406
2a6m <maxime.bourguignon1996@gmail.com>
2017-12-15 11:08:42 +0100

    run test for Function Complex2Polaire

---
+++ FunctionFramework/Complex2Polaire.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperComputer;
+
+namespace FunctionFramework
+{
+    public class Complex2Polaire : Function<string>
+    {
+        public string HelpMessage
+        {
+            get
+            {
+                return "write a Cartesian complex in a Polaire complex";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Pol";
+            }
+        }
+
+        public string[] ParametersName
+        {
+            get
+            {
+                string[] param = new string[2];
+                param.SetValue("R", 0);
+                param.SetValue("Im", 1);
+                return param;
+            }
+        }
+
+        public string Evaluate(string[] args)
+        {
+            try
+            {
+                double x = double.Parse(args[0]);
+                double y = double.Parse(args[1]);
+
+                double arg = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                double azimut = CalculAngle(x, y) / Math.PI;
+
+                // take 2 decimal after coma
+                arg = Math.Round(arg, 2);
+                azimut = Math.Round(azimut, 2);
+
+                if (arg == 0 & azimut == 0)
+                {
+                    return "complex not valid";
+                }
+
+                string ans = string.Format("arg: {0} - az: {1}π", arg, azimut);
+                Console.WriteLine(ans);
+
+                return ans;
+            }
+
+            catch
+            {
+                throw new EvaluationException("Couldn't add the specified double");
+            }
+        }
+
+        private double CalculAngle(double x, double y)
+        {
+            if (x > 0 & y >= 0)
+            {
+                return Math.Atan2(y, x);
+            }
+            else if (x > 0 & y < 0)
+            {
+                return Math.Atan(y / x) + 2 * Math.PI;
+            }
+            else if (x < 0)
+            {
+                return Math.Atan(y / x) + Math.PI;
+            }
+            else if (x == 0 & y > 0)
+            {
+                return Math.PI / 2;
+            }
+            else if (x == 0 & y < 0)
+            {
+                return 3 * Math.PI / 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ FunctionFramework/Complex2PolaireTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionFramework
+{
+    [TestFixture()]
+    class Complex2PolaireTests
+    {
+        [Test()]
+        public void TestC2PEvaluate()
+        {
+            Complex2Polaire C2P = new Complex2Polaire();
+
+            string[] arr1 = new string[] { "2", "2" };
+            string[] arr2 = new string[] { "0", "2" };
+            string[] arr3 = new string[] { "0", "0" };
+            string[] arr4 = new string[] { "-4", "-2" };
+            string[] arr5 = new string[] { "2", "-2" };
+
+            string ans1 = string.Format("arg: {0} - az: {1}π", 2.83, 0.25);
+            string ans2 = string.Format("arg: {0} - az: {1}π", 2, 0.5);
+            string ans3 = "complex not valid";
+            string ans4 = string.Format("arg: {0} - az: {1}π", 4.47, 1.15);
+            string ans5 = string.Format("arg: {0} - az: {1}π", 2.83, 1.75);
+
+            Assert.AreEqual(ans1, C2P.Evaluate(arr1));
+            Assert.AreEqual(ans2, C2P.Evaluate(arr2));
+            Assert.AreEqual(ans3, C2P.Evaluate(arr3));
+            Assert.AreEqual(ans4, C2P.Evaluate(arr4));
+            Assert.AreEqual(ans5, C2P.Evaluate(arr5));
+        }
+    }
+}
